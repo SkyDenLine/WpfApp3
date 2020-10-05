1a51d17a2bc06e0111991b5a726a13c47e455990
Дмитрий Давыдов <mega.davydov@inbox.ru>
2020-10-05 08:28:10 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/ApplicationViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using WpfApp3;
+
+namespace WpfApp3
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        private RelayCommand _addCommand;
+        private RelayCommand _delCommand;
+        private RelayCommand _copyCommand;
+
+        public RelayCommand AddCommand 
+        {
+            get 
+            {
+                return _addCommand ?? (_addCommand= new RelayCommand(obj => 
+                {
+                    Phone phone = new Phone();
+                    Phones.Insert(Phones.Count,phone);
+                    SelectedPhone = phone;
+                }, (obj) => Phones.Count<10 ));;            
+            }                      
+        }
+
+        public RelayCommand DelCommand 
+        {
+            get 
+            {
+                return _delCommand ?? (_delCommand = new RelayCommand(obj =>
+                {
+                    Phone phone = obj as Phone;
+                    if (phone != null)
+                    {
+                        Phones.Remove(phone);
+                    }
+                    
+                }, (obj) => Phones.Count > 0));
+            }
+        }
+
+      
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+                {
+                    new Phone {Title="Phone 1", Company="Company 1", Price = 10000 },
+                    new Phone {Title="Phone 2", Company="Company 2", Price = 20000 },
+                };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfApp3/Phone.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfApp3
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _company;
+        private int _price;
+        private string _description;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Company
+        {
+            get { return _company; }
+            set
+            {
+                _company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public string Description
+        {
+            get 
+            {
+                return _description ;
+            }
+            set 
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged( string prop = "")
+        {           
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
